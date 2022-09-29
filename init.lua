5632b3bc8813821bad3f24d40b9a227c320ed63a
Dmitry Malkov <targtn@gmail.com>
2022-09-28 18:46:26 -0600

    Set initial collapse level for aerial to 1

--- init.lua
+++ init.lua
@@ -305,6 +305,12 @@ local config = {
 
     -- Aerial
     aerial = {
+      on_first_symbols = function(bufnr)
+        -- local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+        -- if ft == "lua" then
+        require("aerial").tree_set_collapse_level(bufnr, 1)
+        -- end
+      end,
       on_attach = function(_, bufnr)
         -- Jump up the tree with '[[' or ']]'
         vim.keymap.set("n", "[[", "<cmd>AerialPrevUp<cr>", { buffer = bufnr, desc = "Jump up and backwards in Aerial" })
