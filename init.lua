543966e8ff31a9f272266c6352a2089555ec3871
Dmitry Malkov <dmitry.malkov@live.com>
2022-11-26 23:00:19 -0600

    Use main branch for nvim-surround

--- init.lua
+++ init.lua
@@ -233,7 +233,6 @@ local config = {
       },
       {
         "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
         config = function()
           require("nvim-surround").setup {
             -- Configuration here, or leave empty to use defaults
